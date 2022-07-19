0b1926f1a13c9abc72fac84dd6c2d7d6de9bd64b
FADEL MAHMOUD B M 1721227 <fadel.mahmoud@live.iium.edu.my>
2022-07-19 21:01:26 +0800

    Filter based on location

--- src/screens/SearchResults/index.js
+++ src/screens/SearchResults/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import {FlatList, View} from 'react-native';
+import {FlatList, View, Text} from 'react-native';
 import React from 'react';
 
 import Post from '../../components/Post';
@@ -10,9 +10,15 @@ const SearchResultsScreen = (props) => {
   const {posts} = props;
 
   console.log("posts" + posts);
+  console.log("posts.length" + posts.length);
   
   return (
-    <View>
+    <View style={{height: '100%', width: '100%' , alignItems:'center' , justifyContent:'center'}}>
+      
+      {posts.length == 0 &&
+        <Text style={{fontSize:25 , alignItems:'center' , justifyContent:'center'}}>No accommodations for selected location</Text>
+      }
+
       <FlatList
         data={posts}
         renderItem={({item}) =>
